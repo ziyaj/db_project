bddea538dcd77a6479f1f7d5edd3fd6700a65f28
ShirleyDi <767464143@qq.com>
2018-03-17 16:35:03 -0700

    Update string to test git function

--- code/src/com/team80/App.java
+++ code/src/com/team80/App.java
@@ -13,7 +13,7 @@ public class App {
         button_msg.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "304 project");
+                JOptionPane.showMessageDialog(null, "304 project initial commit");
             }
         });
     }
