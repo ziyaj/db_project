f80f7356a01a4199be1d1e7af2d7297513b857af
Ziyang Jin <ziyang.jin.master@gmail.com>
2018-03-17 16:18:38 -0700

    Set up initial code

---
+++ code/src/com/team80/App.java
@@ -0,0 +1,28 @@
+package com.team80;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    private JButton button_msg;
+    private JPanel panelMain;
+
+
+    public App() {
+        button_msg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "304 project");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
