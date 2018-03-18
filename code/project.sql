100c44a584444054e0db6d1cc532dc8fa1cb398f
J0YAL <joyal.li@hotmail.com>
2018-03-17 18:20:05 -0700

    New data

--- code/project.sql
+++ code/project.sql
@@ -116,4 +116,123 @@ FOREIGN KEY (traveler_id) REFERENCES Traveler (cid)
 
 grant select on Contract_Signs to public;
 
+insert into University
+values(1, 'UBC');
+
+insert into University
+values(2, 'SFU');
+
+insert into University
+values(3, 'University of Victoria');
+
+insert into University
+values(4, 'Kwantlen Polytechnic University');
+
+insert into University
+values(5, 'University of Toronto');
+
+insert into University
+values(6, 'McGill University');
+
+insert into University
+values(7, 'University of Alberta');
+
+insert into University
+values(8, 'University of Calgary');
+
+insert into University
+values(9, 'University of Manitoba');
+
+insert into University
+values(10, 'University of Waterloo');
+
+insert into Student
+values(1, 'Harry Potter', 'M', 1, 70, 1, 1);
+
+insert into Student
+values(2, 'Cersei Lannister', 'F', 1, 5, 2, 1);
+
+insert into Student
+values(3, 'Guillaume Lux', 'F', 1, 5, 3, 1);
+
+insert into Student
+values(4, 'Jaap Eris', 'M', 1, 5, 4, 1);
+
+insert into Student
+values(5, 'Kidlat Urbanus', 'F', 1, 5, 5, 1);
+
+insert into Student
+values(6, 'Hannah Abbott', 'F', 1, 5, 6, 1);
+
+insert into Student
+values(7, 'Ludo Bagman', 'F', 1, 5, 7, 1);
+
+insert into Student
+values(8, 'Bathilda Bagshot', 'F', 1, 5, 9, 1);
+
+insert into Student
+values(9, 'Katie Bell', 'F', 1, 5, 8, 1);
+
+insert into Student
+values(10, 'Cuthbert Binns', 'F', 1, 5, 10, 1);
+
+insert into Hosts
+values(1, 0, 'V6T1Z4', '2324 Wesbrook Mall');
+
+insert into Hosts
+values(2, 1, 'M5S3G3', '100 St. George St.');
+
+insert into Hosts
+values(3, 0, 'N2L3G1', '200 University Ave W');
+
+insert into Hosts
+values(4, 1, 'H3A0G4', '845 Sherbrooke St W');
+
+insert into Hosts
+values(5, 1, 'L8S4L8', '1280 Main St W');
+
+insert into Hosts
+values(6, 1, 'K7L3N6', '99 University Ave');
+
+insert into Hosts
+values(7, 1, 'V5A1S6', '8888 University Dr');
+
+insert into Hosts
+values(8, 0, 'V8P5C2', '3800 Finnerty Rd');
+
+insert into Hosts
+values(9, 1, 'T6G2R3', '116 St & 85 Ave');
+
+insert into Hosts
+values(10, 0, 'T2N1N4', '2500 University Dr NW');
+
+insert into Traveler
+values(1);
+
+insert into Traveler
+values(2);
+
+insert into Traveler
+values(3);
+
+insert into Traveler
+values(4);
+
+insert into Traveler
+values(5);
+
+insert into Traveler
+values(6);
+
+insert into Traveler
+values(7);
+
+insert into Traveler
+values(8);
+
+insert into Traveler
+values(9);
+
+insert into Traveler
+values(10);
 
