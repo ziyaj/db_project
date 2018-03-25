a45ad2dc10bd123eefd8ef120fb7fa2c957064e9
joyal <joyal.li@hotmail.com>
2018-03-25 11:41:47 -0700

    Implement frontend

--- code/src/com/team80/App.java
+++ code/src/com/team80/App.java
@@ -1,59 +1,335 @@
 package com.team80;
 
+import javax.swing.table.DefaultTableModel;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import org.jdesktop.swingx.JXDatePicker;
+
 
 // before starting the app, you should ssh to your ugrad account
 // start sqlplus, and log in with your own account
 // then run "start project.sql" to make sure our data is stored into the database
 public class App {
-    private JButton button_msg;
-    private JPanel panelMain;
-    private JPasswordField passwordField1;
-    private JTabbedPane tabbedPane1;
-    private JPasswordField host_password;
-    private JTextField host_username;
-    private JTextField textField2;
-    private JButton submitButton;
-    private JPasswordField passwordField3;
-    private JButton submitButton2;
-    private JTextField textField3;
-    private static PersistenceLayer persistenceLayer;
+
+    //<editor-fold desc="Fields">
+    JPanel panelMain;
+    JTabbedPane tab;
+    private JTextField aidTextField;
+    private JPasswordField aPasswordField;
+    private JButton adminSignUpButton;
+    private JButton adminSignInButton;
+    private JButton hSignUpButton;
+    private JButton hSignInButton;
+    private JButton tSignUpButton;
+    private JButton tSignInButton;
+    private JButton hUpdateButton;
+    private JButton hDeleteButton;
+    private JButton hSearchButton;
+    private JButton hInsertButton;
+    private JTextField PID;
+    private JTextField descriptionTextField;
+    private JTable hTable;
+    private JTextField pidTextField;
+    private JButton logOutButton;
+    private JTextField hostMsg;
+    static PersistenceLayer persistenceLayer;
+    private JTextField hidTextField;
+    private JTextField hLoginTextField;
+    private JPanel adminPanel;
+    private JPanel hostPanel;
+    private JPanel travellerPanel;
+    private JPanel hostEditorPanel;
+    private JPasswordField hPasswordField;
+    private JTextField roomTextField;
+    private JTextField residenceTextField;
+    private JRadioButton checkedRadioButton;
+    private JTextField dailyRateTextField;
+    private JTextField rTextField;
+    private JPasswordField rPasswordField;
+    private JButton rSignUpButton;
+    private JTextField fromDateTextField;
+    private JPanel toDatePanel;
+    private JXDatePicker toDatePicker;
+    private JPanel fromDatePanel;
+    private JXDatePicker fromDatePicker;
+    private JTextField tidTextField;
+    private JPasswordField tPasswordField;
+    private JPanel registerPanel;
+    private int hid;
+
+    DefaultTableModel model = new DefaultTableModel();
+    //</editor-fold>
 
     public App() {
 
-        button_msg.addActionListener(new ActionListener() {
+        //<editor-fold desc="Init">
+        panelMain.setPreferredSize(new Dimension(700, 300));
+
+        String[] hTableColumns = {"PID", "From_Date", "To_Date", "Description"};
+        model.setColumnIdentifiers(hTableColumns);
+        hTable.setModel(model);
+        hTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+        hTable.setFillsViewportHeight(true);
+
+        JPanel adminLogin = (JPanel) tab.getComponentAt(0);
+        JPanel hostLogin = (JPanel) tab.getComponentAt(1);
+        JPanel travellerLogin = (JPanel) tab.getComponentAt(2);
+        JPanel hostEditor = (JPanel) tab.getComponentAt(3);
+        JPanel register = (JPanel) tab.getComponentAt(4);
+        // Hide the HostEditor tab on Login window
+        tab.remove(hostEditor);
+        tab.remove(register);
+        //</editor-fold>
+
+        //<editor-fold desc="Host Editor Events">
+        logOutButton.addMouseListener(new MouseAdapter() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "304 project initial commit");
-                SQLUtil.connectOracle();
-                persistenceLayer.closeConnection(); // actually this should be called when we close the app,
-                // but there is not such button yet
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                tab.add(hostLogin, 1);
+                tab.setTitleAt(1, "Host");
+                tab.remove(hostEditor);
+                tab.setSelectedIndex(1);
             }
         });
-
-        submitButton.addActionListener(new ActionListener() {
+        hSearchButton.addMouseListener(new MouseAdapter() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                String username = host_username.getText();
-                String password = host_password.getText();
-                if (username.equals("leo") && password.equals("sb")) {
-                    JOptionPane.showMessageDialog(null, "Login successfully");
-                    JFrame host = new JFrame("host");
-                    host.setVisible(true);
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                search();
+            }
+        });
+        hUpdateButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                int[] selectedRows = getSelected();
+                if (selectedRows.length == 0) {
+                    JOptionPane.showMessageDialog(null, "Please select a record", "Update Message", JOptionPane.WARNING_MESSAGE);
+                    return;
+                } else if (selectedRows.length > 1) {
+                    JOptionPane.showMessageDialog(null, "Please select only one record at a time", "Update Message", JOptionPane.WARNING_MESSAGE);
+                } else {
+                    Date fromDate = (fromDatePicker.getDate() == null) ? null : new Date(fromDatePicker.getDate().getTime());
+                    Date toDate = (toDatePicker.getDate() == null) ? null : new Date(toDatePicker.getDate().getTime());
+                    String description = descriptionTextField.getText();
+                    int dialogResult = JOptionPane.showConfirmDialog (null, "Update selected record?","Warning", JOptionPane.YES_NO_OPTION);
+                    if(dialogResult == JOptionPane.YES_OPTION) {
+                        int result = SQLUtil.updatePost(selectedRows, model, fromDate, toDate, description);
+                        search();
+                        if (result > 0) {
+                            JOptionPane.showMessageDialog(null, "Update successful.", "Update Message", JOptionPane.INFORMATION_MESSAGE);
+                            tab.remove(register);
+                        } else if (result == 0) {
+                            JOptionPane.showMessageDialog(null, "Invalid period", "Update Message", JOptionPane.ERROR_MESSAGE);
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Update failed. Please try again", "Update Message", JOptionPane.ERROR_MESSAGE);
+                        }
+                    }
                 }
             }
         });
+        hInsertButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                if (fromDatePicker.getDate() == null || toDatePicker.getDate() == null) {
+                    JOptionPane.showMessageDialog(null, "Missing date", "New Posting Message", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+                Date fromDate = new Date(fromDatePicker.getDate().getTime());
+                Date toDate = new Date(toDatePicker.getDate().getTime());
+                if(fromDate.after(toDate)){
+                    JOptionPane.showMessageDialog(null, "Invalid period", "Update Message", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+
+                String description = descriptionTextField.getText();
+                int result = SQLUtil.addPost(hid, fromDate, toDate, description);
+                if(result == 1) {
+                    JOptionPane.showMessageDialog(null, "New posting has been added", "New Posting Message", JOptionPane.INFORMATION_MESSAGE);
+                    tab.remove(register);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Insert failed. Please try again.", "New Posting Message", JOptionPane.ERROR_MESSAGE);
+                }
+                search();
+            }
+        });
+        hDeleteButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                int[] selectedRows = getSelected();
+                if (selectedRows.length == 0) {
+                    JOptionPane.showMessageDialog(null, "Please select a record", "Deletion Message", JOptionPane.WARNING_MESSAGE);
+                    return;
+                }
+                int dialogResult = JOptionPane.showConfirmDialog (null, "Delete selected records?","Warning", JOptionPane.YES_NO_OPTION);
+                if(dialogResult == JOptionPane.YES_OPTION){
+                    int[] result = SQLUtil.deletePosts(selectedRows, model);
+                    hSearchButton.doClick();
+                    search();
+                    if(result.length > 0) {
+                        JOptionPane.showMessageDialog(null, "Selected records have been removed.", "Deletion Message", JOptionPane.INFORMATION_MESSAGE);
+                        tab.remove(register);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Deletion failed. Please try again.", "Deletion Message", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            }
+        });
+        //</editor-fold>
+
+        //<editor-fold desc="Register Events">
+        rSignUpButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                int cID;
+                boolean isChecked;
+                String roomNo = "";
+                String residence = "";
+                int dailyRate = -1;
+                if ((cID = isIdValid(rTextField.getText())) != -1) {
+                    ResultSet rs = SQLUtil.getHost(cID);
+                    try {
+                        if (rs.next()) {
+                            JOptionPane.showMessageDialog(null, "User already exists", "Register Message", JOptionPane.ERROR_MESSAGE);
+                            return;
+                        }
+                    } catch (SQLException e1) { }
+
+                    roomNo = roomTextField.getText();
+                    residence = residenceTextField.getText();
+                    isChecked = checkedRadioButton.isSelected();
+                    try {
+                        dailyRate = Integer.parseInt(dailyRateTextField.getText());
+                    } catch (Exception e1) {
+                        JOptionPane.showMessageDialog(null, "Invalid daily rate","Error Message", JOptionPane.ERROR_MESSAGE);
+                    }
+                    if (roomNo.equals("") || residence.equals("") || dailyRate == -1) {
+                        JOptionPane.showMessageDialog(null, "Info missing","Error Message", JOptionPane.ERROR_MESSAGE);
+                        return;
+                    }
+                    int result = SQLUtil.addHost(cID, isChecked, roomNo, residence, dailyRate);
+                    if(result == 1) {
+                        JOptionPane.showMessageDialog(null, "Registration successful. Please log in.", "Register Message", JOptionPane.INFORMATION_MESSAGE);
+                        tab.remove(register);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Registration failed. Please try again", "Register Message", JOptionPane.ERROR_MESSAGE);
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid username. No such student","Error Message", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        //</editor-fold>
+
+        //<editor-fold desc="Host Events">
+        hSignUpButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                tab.add(register);
+                tab.setTitleAt(3, "Register");
+                tab.setSelectedIndex(3);
+            }
+        });
+        hSignInButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                try {
+                    hid = Integer.parseInt(hidTextField.getText());
+                } catch(Exception e1) {
+                    hLoginTextField.setText("Invalid username");
+                    return;
+                }
+                ResultSet rs = SQLUtil.getHost(hid);
+
+                try {
+                    if (rs.next()) {
+                        char[] pw = hPasswordField.getPassword();
+                        if (hidTextField.getText().equals(String.valueOf(pw))) {
+                            tab.add(hostEditor, 1);
+                            tab.setTitleAt(1, "HostEditor");
+                            tab.remove(hostLogin);
+                            tab.setSelectedIndex(1);
+                        } else {
+                            hLoginTextField.setText("Invalid password. Try again.");
+                            JOptionPane.showMessageDialog(null, "Invalid password. Try again.","Error Message", JOptionPane.ERROR_MESSAGE);
+                        }
+                    } else {
+                        hid = -1;
+                        hLoginTextField.setText("User does not exist. Please sign up first.");
+                    }
+                } catch (SQLException e2) {
+                    e2.printStackTrace();
+                }
+            }
+        });
+        //</editor-fold>
     }
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("App");
-        frame.setContentPane(new App().panelMain);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-        persistenceLayer = PersistenceLayer.getInstance();
+    //<editor-fold desc="Helper">
+    public int isIdValid(String id) {
+        int cid = -1;
+        try {
+            cid = Integer.parseInt(id);
+            ResultSet rs = SQLUtil.getStudent(cid);
+            cid = rs.next() ? cid : -1;
+        } catch (Exception e1) {
+        }
+        return cid;
     }
 
+
+    public void search() {
+        int pid;
+        ResultSet rs = null;
+        String pidStr = pidTextField.getText();
+        if (!pidStr.equals("")) {
+            try {
+                pid = Integer.parseInt(pidStr);
+                // TODO: add pid as search criterion
+
+            } catch (Exception e1) {
+                hLoginTextField.setText("Invalid username");
+                return;
+            }
+        } else {
+            rs = SQLUtil.findAllPosts(hid);
+        }
+
+        try {
+            int pID;
+            Date fromDate;
+            Date toDate;
+            String description = "";
+            model.setRowCount(0);
+            while (rs.next()) {
+                pID = rs.getInt(1);
+                fromDate = rs.getDate("fromdate");
+                toDate = rs.getDate("todate");
+                description = rs.getString("description");
+                Object[] row = {pID, fromDate, toDate, description};
+                model.addRow(row);
+            }
+        } catch (SQLException e1) { }
+    }
+
+    public int[] getSelected() {
+        int[] selection = hTable.getSelectedRows();
+        for (int i = 0; i < selection.length; i++) {
+            selection[i] = hTable.convertRowIndexToModel(selection[i]);
+        }
+        return selection;
+    }
+    //</editor-fold>
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
--- code/src/com/team80/SQLUtil.java
+++ code/src/com/team80/SQLUtil.java
@@ -1,9 +1,7 @@
 package com.team80;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.swing.table.DefaultTableModel;
+import java.sql.*;
 
 public class SQLUtil {
 
@@ -30,14 +28,14 @@ public class SQLUtil {
         }
     }
 
-    public static ResultSet findAllPosts() {
+    public static ResultSet findAllPosts(int id) {
         try {
             final PersistenceLayer persistenceLayer = PersistenceLayer.getInstance();
             final Connection con = persistenceLayer.getConnection();
-            final Statement stmt = con.createStatement();
-            // stmt is a statement object
-            final ResultSet rs = stmt.executeQuery("SELECT * FROM Posting");
-            return rs;
+            String stmt = (id == -1) ? "SELECT * FROM Posting" : "SELECT * FROM Posting WHERE hostid = ? ORDER BY pid DESC";
+            PreparedStatement ps = con.prepareStatement(stmt);
+            ps.setInt(1, id);
+            return ps.executeQuery();
         } catch(final SQLException e) {
             System.err.println("An error occurred while executing query.");
             System.err.println(e.getMessage());
@@ -45,4 +43,119 @@ public class SQLUtil {
         return null;
     }
 
+    public static int[] deletePosts(int[] selection, DefaultTableModel model) {
+        try {
+            final PersistenceLayer persistenceLayer = PersistenceLayer.getInstance();
+            final Connection con = persistenceLayer.getConnection();
+            PreparedStatement ps = con.prepareStatement("DELETE FROM Posting WHERE pid = ?");
+            for (int i = 0; i < selection.length; i++) {
+                int pid = (int)model.getValueAt(selection[i], 0);
+                ps.setInt(1, pid);
+                ps.addBatch();
+            }
+            return ps.executeBatch();
+        } catch(final SQLException e) {
+            System.err.println("An error occurred while executing query.");
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public static int updatePost(int[] selection, DefaultTableModel model, Date fromDate, Date toDate, String description) {
+        try {
+            final PersistenceLayer persistenceLayer = PersistenceLayer.getInstance();
+            final Connection con = persistenceLayer.getConnection();
+
+            Date fDate = (fromDate == null) ? (Date) model.getValueAt(selection[0], 1) : fromDate;
+            Date tDate = (toDate == null) ? (Date) model.getValueAt(selection[0], 2) : toDate;
+
+            if(fDate.after(tDate)){
+                return 0;
+            }
+
+            String stmt = "UPDATE Posting SET description = ? , fromdate = ? , todate = ? WHERE pid = ?";
+            PreparedStatement ps = con.prepareStatement(stmt);
+            int pid = (int)model.getValueAt(selection[0], 0);
+            ps.setString(1, description);
+            ps.setDate(2, fDate);
+            ps.setDate(3, tDate);
+            ps.setInt(4, pid);
+            return ps.executeUpdate();
+        } catch(final SQLException e) {
+            System.err.println("An error occurred while executing query.");
+            System.err.println(e.getMessage());
+        }
+        return -1;
+    }
+
+    public static int addPost(int id, Date fromDate, Date toDate, String description) {
+        try {
+            final PersistenceLayer persistenceLayer = PersistenceLayer.getInstance();
+            final Connection con = persistenceLayer.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT MAX(pid) FROM Posting");
+            ResultSet rs = ps.executeQuery();
+            int count = 0;
+            if (rs.next()) {
+                count = rs.getInt(1);
+            }
+            System.out.println("count: " + count);
+            ps = con.prepareStatement("INSERT INTO Posting VALUES (?, ?, ?, ?, ?)");
+            ps.setInt(1, count + 1);
+            ps.setDate(2, fromDate);
+            ps.setDate(3, toDate);
+            ps.setString(4, description);
+            ps.setInt(5, id);
+            return ps.executeUpdate();
+        } catch(final SQLException e) {
+            System.err.println("An error occurred while executing query.");
+            System.err.println(e.getMessage());
+        }
+        return -1;
+    }
+
+    public static ResultSet getHost(int id) {
+        try {
+            final PersistenceLayer persistenceLayer = PersistenceLayer.getInstance();
+            final Connection con = persistenceLayer.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM Hosts WHERE cid = ?");
+            ps.setInt(1, id);
+            return ps.executeQuery();
+        } catch(final SQLException e) {
+            System.err.println("An error occurred while executing query.");
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public static int addHost(int cid, boolean isChecked, String roomNo, String residence, int dailyRate) {
+        try {
+            final PersistenceLayer persistenceLayer = PersistenceLayer.getInstance();
+            final Connection con = persistenceLayer.getConnection();
+            PreparedStatement ps = con.prepareStatement("INSERT INTO Hosts VALUES (?, ?, ?, ?, ?)");
+            ps.setInt(1, cid);
+            ps.setBoolean(2, isChecked);
+            ps.setString(3, roomNo);
+            ps.setString(4, residence);
+            ps.setInt(5, dailyRate);
+            return ps.executeUpdate();
+        } catch(final SQLException e) {
+            System.err.println("An error occurred while executing query.");
+            System.err.println(e.getMessage());
+        }
+        return -1;
+    }
+
+    public static ResultSet getStudent(int id) {
+        try {
+            final PersistenceLayer persistenceLayer = PersistenceLayer.getInstance();
+            final Connection con = persistenceLayer.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM Student WHERE cid = ?");
+            ps.setInt(1, id);
+            return ps.executeQuery();
+        } catch(final SQLException e) {
+            System.err.println("An error occurred while executing query.");
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
 }
